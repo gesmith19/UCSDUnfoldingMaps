4d658c05e5564f8b7d068a3e8f2f5f0e7ddd56c8
gesmith19 <gesmith2@gmail.com>
2016-02-21 20:37:43 +0000

    extension project - airports and routes
    Created applet to display airports and routes based around the same
    logic as the Earthquake map.

--- src/module6/AirportMap.java
+++ src/module6/AirportMap.java
@@ -1,6 +1,7 @@
 package module6;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,6 +12,8 @@ import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.utils.MapUtils;
+
+
 import de.fhpotsdam.unfolding.geo.Location;
 import parsing.ParseFeed;
 import processing.core.PApplet;
@@ -21,11 +24,18 @@ import processing.core.PApplet;
  * MOOC team
  *
  */
+@SuppressWarnings("unused")
 public class AirportMap extends PApplet {
 	
 	UnfoldingMap map;
 	private List<Marker> airportList;
 	List<Marker> routeList;
+	
+	private CommonMarker lastSelected;
+	private CommonMarker lastClicked;
+	
+	private HashMap<Integer, AirportMarker> airportRoutes;
+	private static final long serialVersionUID = 1L;
 	
 	public void setup() {
 		// setting up PAppler
@@ -41,16 +51,18 @@ public class AirportMap extends PApplet {
 		// list for markers, hashmap for quicker access when matching with routes
 		airportList = new ArrayList<Marker>();
 		HashMap<Integer, Location> airports = new HashMap<Integer, Location>();
+		airportRoutes = new HashMap<Integer, AirportMarker>();
 		
 		// create markers from features
-		for(PointFeature feature : features) {
-			AirportMarker m = new AirportMarker(feature);
+		for ( PointFeature feature : features ) {
+			AirportMarker m = new AirportMarker( feature );
 	
 			m.setRadius(5);
 			airportList.add(m);
 			
 			// put airport in hashmap with OpenFlights unique id for key
 			airports.put(Integer.parseInt(feature.getId()), feature.getLocation());
+			airportRoutes.put( Integer.parseInt( feature.getId() ), m);
 		
 		}
 		
@@ -71,17 +83,26 @@ public class AirportMap extends PApplet {
 			}
 			
 			SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
-		
-			System.out.println(sl.getProperties());
+			sl.setHidden( true );
+		    
+//			System.out.println(sl.getProperties());
 			
 			//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
-			//routeList.add(sl);
+			
+			 routeList.add(sl);
+			
+			 // this adds the routes to the routes in the airportmarker instance
+			if ( airportRoutes.containsKey( source ) && airportRoutes.containsKey( dest ) ) {
+				airportRoutes.get( source ).addRoute( sl );
+				airportRoutes.get( dest ).addRoute( sl );
+			}
 		}
 		
+		sortAndPrint(20);
 		
 		
 		//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
-		//map.addMarkers(routeList);
+		map.addMarkers(routeList);
 		
 		map.addMarkers(airportList);
 		
@@ -93,5 +114,113 @@ public class AirportMap extends PApplet {
 		
 	}
 	
+	private void sortAndPrint(int numToPrint) {
+		List<AirportMarker> alm = new ArrayList<AirportMarker>();
+		for ( Marker m : airportList ) {
+			AirportMarker am = ( AirportMarker )m;
+			alm.add( am );
+		}
+		Collections.sort( alm, Collections.reverseOrder() );
+		int max = ( numToPrint >= alm.size() ? alm.size() : numToPrint ); 
+		for ( int i = 0; i < max; i++ ) {
+			System.out.println(alm.get( i ) );
+		}
+	}
 
+	
+	/** Event handler that gets called automatically when the 
+	 * mouse moves.
+	 */
+	@Override
+	public void mouseMoved()
+	{
+		// clear the last selection
+		if (lastSelected != null) {
+			lastSelected.setSelected(false);
+			lastSelected = null;
+		
+		}
+		selectMarkerIfHover( airportList );
+
+		//loop();
+	}
+
+	// If there is a marker selected 
+	private void selectMarkerIfHover(List<Marker> markers)
+	{
+		// Abort if there's already a marker selected
+		if (lastSelected != null) {
+			return;
+		}
+		
+		for (Marker m : markers) {
+			CommonMarker marker = (CommonMarker)m;
+			if (marker.isInside(map,  mouseX, mouseY)) {
+				lastSelected = marker;
+				marker.setSelected(true);
+				return;
+			}
+		}
+	}
+	
+	/** The event handler for mouse clicks
+	 * It will display an airport and lines for the routes out of and into the airport
+	 */
+	@Override
+	public void mouseClicked()
+	{
+		if (lastClicked != null) {
+			displayDefaultMarkers();
+			lastClicked = null;
+		}
+		else if (lastClicked == null) {
+			checkAirportsForClick();
+		}
+	}
+	
+/*
+ * default setting for markers.  Airports are displayed and routes are hidden	
+ */
+	private void displayDefaultMarkers() {
+		
+		for ( Marker m : airportList ) {
+			m.setHidden( false );
+		}
+		
+		for ( Marker m : routeList ) {
+			m.setHidden( true );
+		}
+	}
+	
+		
+	private void checkAirportsForClick() {
+		if (lastClicked != null) return;
+		
+		for (Marker marker : airportList) {
+		if (!marker.isHidden() && marker.isInside(map, mouseX, mouseY)) {
+			lastClicked = (CommonMarker)marker;
+	//		System.out.println(marker.getProperties());
+			
+				
+				// hide the other airports
+				for (Marker mk : airportList) {
+				if (mk != lastClicked) {
+					mk.setHidden(true);
+				}
+			}
+				
+				// display the routes for selected airport
+				int count = 0;
+				for (Marker sl : ((AirportMarker)lastClicked).routes) {
+				sl.setHidden(false);
+		//		System.out.println(sl.getProperties());
+				count += 1;
+				
+				}
+				System.out.println( "number of routes displayed: " + count);
+				
+			return;
+			}
+		}
+	} // end of checkAirportsForClick()
 }
--- src/module6/AirportMarker.java
+++ src/module6/AirportMarker.java
@@ -1,10 +1,12 @@
 package module6;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
 import processing.core.PGraphics;
 
 /** 
@@ -14,29 +16,120 @@ import processing.core.PGraphics;
  * MOOC team
  *
  */
-public class AirportMarker extends CommonMarker {
-	public static List<SimpleLinesMarker> routes;
+public class AirportMarker extends CommonMarker implements Comparable<AirportMarker> {
+	public  List<SimpleLinesMarker> routes;
 	
 	public AirportMarker(Feature city) {
 		super(((PointFeature)city).getLocation(), city.getProperties());
-	
+		routes = new ArrayList<SimpleLinesMarker>();
 	}
 	
 	@Override
 	public void drawMarker(PGraphics pg, float x, float y) {
-		pg.fill(11);
-		pg.ellipse(x, y, 5, 5);
-		
-		
+		colourDetermine( pg );
+		pg.ellipse( x, y, 5, 5 );
 	}
 
 	@Override
 	public void showTitle(PGraphics pg, float x, float y) {
-		 // show rectangle with title
+		// show rectangle with title
 		
-		// show routes
+		String title1 = getCode() + " " + getName();
+		String title2 = getCity() + " " + getCountry();
+		String title3 = "Number of routes: " + getRouteNumber();
+		float w1 = pg.textWidth( title1 );
+		float w2 = pg.textWidth( title2 );
+		float w3 = pg.textWidth( title3 );
 		
+		float w = 0;
 		
+		// work out max width
+		if ( w1 > w2 && w1 > w3 ){
+			w = w1;	
+		}
+		else if ( w2 > w1 && w2 > w3 ){
+			w = w2;
+		}
+		else if ( w3 > w1 && w3 > w2 ) {
+			w = w3;
+		}
+		else
+			w = w1;
+		
+		pg.pushStyle();
+		
+		// create the rectangle
+        pg.stroke( 110 );
+        pg.fill( 234, 235,213 );
+        pg.rect( x, y + 10, w + 8, 54, 7 );
+       
+        // write the text in the rectangle
+        pg.textAlign(PConstants.LEFT, PConstants.TOP);
+        pg.fill( 0 );
+        pg.textSize( 12 );
+        pg.text( title1, x + 3 , y + 12 );
+        pg.text( title2, x + 3 , y + 28 );
+        pg.text( title3, x + 3, y + 44 );
+        
+     // Restore previous drawing style
+     		pg.popStyle();
 	}
 	
+	// determine color of marker from depth
+	// We use: Deep = red, intermediate = blue, shallow = yellow
+	private void colourDetermine( PGraphics pg ) {
+		
+		// get the number of routes
+		int num = getRouteNumber();
+					
+		if ( num < 20 ) {
+				pg.fill( 255, 255, 0 );
+		}
+		else if ( num < 50 ) {
+				pg.fill( 0, 0, 255 );
+		}
+		else {
+				pg.fill( 255, 0, 0 );
+		}
+	}
+	
+	private int getRouteNumber() {
+		return routes.size();
+	}
+	
+	public void addRoute(SimpleLinesMarker sl) {
+		routes.add( sl );
+	}
+	
+	public int compareTo(AirportMarker marker) {
+		
+		int thisRoutes = this.getRouteNumber();
+		int otherRoutes = marker.getRouteNumber();
+		int cmp = thisRoutes > otherRoutes ? +1 : thisRoutes < otherRoutes ? -1 : 0;
+		return cmp;
+	}
+	
+	// getter methods
+	
+	public String getName() {
+		return getProperty( "name" ).toString();
+	}
+	
+	public String getCode() {
+		return getProperty( "code" ).toString();
+	}
+	
+	public String getCity() {
+	    return getProperty( "city" ).toString();
+	}
+	
+	public String getCountry() {
+		return getProperty( "country" ).toString();
+	}
+	
+	public String toString() {
+		String inf = getCity() + " " + getCode() + " " + getName() + " " + getCountry();
+		inf = inf + " " + "routes: " + getRouteNumber();
+		return inf;
+    }
 }
